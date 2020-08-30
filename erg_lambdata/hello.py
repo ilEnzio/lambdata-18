4e7301076df329af4b2f14b5c5aba2a17618cc8b
ilenzio <westfury@gmail.com>
2020-08-30 16:35:15 -0400

    Configured virtual environment, added somepython files, and configured with init file

---
+++ erg_lambdata/hello.py
@@ -0,0 +1,9 @@
+
+
+# Todo: import enlarge
+
+from erg_lambdata.myMod import enlarge
+# Todo: invoke enlarge
+print("hello")
+
+print(enlarge(8))
---
+++ erg_lambdata/myMod.py
@@ -0,0 +1,18 @@
+
+# myMod
+
+def enlarge(n):
+    # Take and int and enlarge it
+    return n * 100
+
+
+# y = int(input("Please input a number: "))
+# print(f"{y}, {enlarge(y)}")
+
+
+if __name__ == "__main__":
+    # oly run the code below IF this script is
+    # not if it is imported from anaother script
+    print("HELLO")
+    y = int(input("Please choose a number"))
+    print(y, enlarge(y))
